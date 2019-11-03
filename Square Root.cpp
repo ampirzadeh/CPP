59ab8ed058d1a1ade2e47cc75c3c70e58d9e0cf9
ampirzadeh <am.pirzadeh@gmail.com>
2019-11-03 17:53:33 +0330

    update

---
+++ Square Root.cpp
@@ -0,0 +1,11 @@
+#include <iostream>  
+#include<cmath>
+using namespace std;  
+int main()
+{
+    float arg=0;
+    cout << "Enter Number: ";
+    cin >> arg;
+    cout << "Square root of "<< arg <<" is " << sqrt(arg) << "." << endl;
+    return 0;  
+}
