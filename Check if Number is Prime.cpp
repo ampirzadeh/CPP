0d1eefc4e4ed3ee1ab9a1e23c83111f4f7f0aafd
ampirzadeh <50584655+ampirzadeh@users.noreply.github.com>
2019-12-04 13:51:59 -0500

    Update Check if Number is Prime.cpp

--- Check if Number is Prime.cpp
+++ Check if Number is Prime.cpp
@@ -1,15 +1,19 @@
 #include <iostream>  
 using namespace std;  
-int x,i=2,y;
+int x,i=2;
+bool isPrime=true;
 int main()  
 {  
   cout << "Enter Number: ";
   cin >> x;
-  while (i<x){
-      y=x%i;
-      i++;
+  while (i<=x/2){
+      if (x%i==0){;
+      isPrime=false;
+      break;
+      }
+    else {i++;}
   }
-    if (y!=0){cout<< "Prime";}
-    else {cout<<"Not Prime";}
+    if (isPrime){cout<<"Prime";}
+    else { cout<<"Not Prime";}
   return 0;  
 }
