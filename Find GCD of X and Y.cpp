292f888e99e56321de3ad44c080da0afab4d1d9c
ampirzadeh <50584655+ampirzadeh@users.noreply.github.com>
2019-12-04 09:38:10 -0500

    Create Find GCD of X and Y.cpp

---
+++ Find GCD of X and Y.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int x,y;
+int main() {
+   cout << "Enter first number: ";
+   cin >> x;
+   cour << "Enter second number: ";
+   cin >> y;
+   cout<<"GCD of "<< x <<" and "<< y <<" is "<< (y, x % y); 
+   return 0; 
+}   
